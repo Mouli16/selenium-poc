5c2db1cd29bd05a8515041a17fc85bf25634e809
Mouli Sarkar <Mouli Sarkar@192.168.0.8>
2021-05-18 09:34:53 +0530

    grid commit

---
+++ src/RemoteRunning.java
@@ -0,0 +1,21 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class RemoteRunning {
+	
+	public static void main(String args[]) throws MalformedURLException
+	{
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),dc);
+		driver.get("http://www.google.com");
+	}
+
+}
