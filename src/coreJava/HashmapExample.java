9a926a5e9eee3b506148b9f9eedf467ef8fb9a1a
Mouli Sarkar <Mouli Sarkar@192.168.0.8>
2021-05-23 22:16:29 +0530

    commit hashmap

--- src/coreJava/HashmapExample.java
+++ src/coreJava/HashmapExample.java
@@ -1,10 +1,31 @@
 package coreJava;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class HashmapExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+     HashMap<Integer,String> hm = new HashMap<Integer,String>();
+     hm.put(0,"cat");
+     hm.put(1,"dog");
+     hm.put(2,"mouse");
+     System.out.println(hm.get(2));
+     System.out.println(hm.remove(0));
+     
+        Set s= hm.entrySet();
+         Iterator it=   s.iterator();
+         //hashtable- pass table set collections
+         while(it.hasNext())
+         {
+        	 Map.Entry mp = (Map.Entry)it.next();
+        	 System.out.println(mp.getKey());
+        	 System.out.println(mp.getValue());
+         }
 	}
+	
 
 }
